1f2dcd226c977ac0dbb00e16cebdfb37c15fb216
Honglun Wu <49599932+wuhonglun@users.noreply.github.com>
2019-04-13 18:17:38 -0700

    Add new file
    Add a Java file to the reposutory.

---
+++ Practice.java
@@ -0,0 +1,11 @@
+
+public class Practice {
+	
+	//Calculate the average of two numbers
+	public static void main(String[] args) {
+		double a = 123.45;
+		double b = 678.90;
+		double c = a + b/2;
+		System.out.println("The average of "+a+" and "+b+" is "+ c + ".");
+	}
+}
