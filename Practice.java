39e9b16fe378121da1bff047d97e89f824cba467
xupanqian0429 <49603493+xupanqian0429@users.noreply.github.com>
2019-04-13 21:39:20 -0700

    modify formula for variable c
    formula correction

--- Practice.java
+++ Practice.java
@@ -5,7 +5,7 @@ public class Practice {
 	public static void main(String[] args) {
 		double a = 123.45;
 		double b = 678.90;
-		double c = a + b/2;
+		double c = (a + b)/2;
 		String output = "The mean of " + a + " and " + b + " is " + c + ".";
 		System.out.println(output);
 	}
